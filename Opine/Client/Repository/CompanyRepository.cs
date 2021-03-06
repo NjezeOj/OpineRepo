0d24eb4d61d20293759d2b252484630c2b74df06
NjezeOj <47678586+NjezeOj@users.noreply.github.com>
2021-03-06 02:47:54 +0100

    New Commit

---
+++ Opine/Client/Repository/CompanyRepository.cs
@@ -0,0 +1,41 @@
+﻿using Opine.Client.Helpers;
+using Opine.Shared.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Opine.Client.Repository
+{
+    public class CompanyRepository: ICompanyRepository
+    {
+        public readonly IHttpService httpService;
+        private string url = "api/company";
+
+        public CompanyRepository(IHttpService httpService)
+        {
+            this.httpService = httpService;
+        }
+
+        public async Task<List<Company>> GetCompanies()
+        {
+            var response = await httpService.Get<List<Company>>(url);
+
+            if (!response.Success)
+            {
+                throw new ApplicationException(await response.GetBody());
+            }
+
+            return response.Response;
+        }
+
+        public async Task CreateCompany(Company company)
+        {
+            var response = await httpService.Post(url, company);
+            if (!response.Success)
+            {
+                throw new ApplicationException(await response.GetBody());
+            }
+        }
+    }
+}
