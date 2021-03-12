6cadf54b22a5b8187b997d5a262fccf35b6ad0b3
NjezeOj <47678586+NjezeOj@users.noreply.github.com>
2021-03-12 14:58:52 +0100

    New Controller

---
+++ Opine/Server/Controllers/AccountsController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Opine.Shared.DTOS;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opine.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountsController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly IConfiguration _configuration;
+
+        public AccountsController(
+            UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager,
+            IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _configuration = configuration;
+        }
+
+        [HttpPost("Create")]
+        public async Task<ActionResult<UserToken>> CreateUser([FromBody] UserInfo model)
+        {
+            var user = new IdentityUser { UserName = model.FullName, Email = model.Email };
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                return BuildToken(model);
+            }
+            else
+            {
+                return BadRequest("FullName or Password Invalid");
+            }
+        }
+
+        [HttpPost("Login")]
+        public async Task<ActionResult<UserToken>> Login([FromBody] UserInfo userInfo)
+        {
+            var result = await _signInManager.PasswordSignInAsync(userInfo.Email,
+                userInfo.Password, isPersistent: false, lockoutOnFailure: false); //lockoutOnFailure: true for production
+
+            if (result.Succeeded)
+            {
+                return BuildToken(userInfo);
+            }
+            else
+            {
+                return BadRequest("Invalid Login Details");
+            }
+        }
+
+        private UserToken BuildToken(UserInfo userinfo)
+        {
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, userinfo.FullName),
+                new Claim(ClaimTypes.Email, userinfo.Email)
+
+            };
+
+            //configuring credentials used to sign in our jwt 
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var expiration = DateTime.UtcNow.AddYears(1);
+
+            JwtSecurityToken token = new JwtSecurityToken(
+                issuer: null,
+                audience: null,
+                claims: claims,
+                expires: expiration,
+                signingCredentials: creds);
+
+            return new UserToken()
+            {
+                Token = new JwtSecurityTokenHandler().WriteToken(token),
+                Expiration = expiration
+            };
+        }
+    }
+}
