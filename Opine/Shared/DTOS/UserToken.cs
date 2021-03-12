5f8fdab734cbd2cfb92d42f450f5f7983d5c7104
NjezeOj <47678586+NjezeOj@users.noreply.github.com>
2021-03-12 14:58:05 +0100

    New Commit

---
+++ Opine/Shared/DTOS/UserToken.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Opine.Shared.DTOS
+{
+    public class UserToken
+    {
+        public string Token { get; set; }
+        public DateTime Expiration { get; set; }
+    }
+}
