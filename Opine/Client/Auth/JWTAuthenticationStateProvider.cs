9df832b4c43503e97bce9b99c25fcbee64d66fc7
NjezeOj <47678586+NjezeOj@users.noreply.github.com>
2021-03-15 10:21:30 +0100

    AuthenticationState Provider

---
+++ Opine/Client/Auth/JWTAuthenticationStateProvider.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.JSInterop;
+using Opine.Client.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Opine.Client.Auth
+{
+    public class JWTAuthenticationStateProvider : AuthenticationStateProvider, ILoginService
+    {
+        private readonly IJSRuntime js;
+        private HttpClient httpClient;
+        private readonly string TOKENKEY = "TOKENKEY";
+        private AuthenticationState anonymous =>
+            new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+
+        //configure the the httpClient service so that every time we communicate with the web api we return a web api we want to send the jwt so that we get authenticated in the asp.net core app
+        public JWTAuthenticationStateProvider(IJSRuntime js, HttpClient httpClient)
+        {
+            this.js = js;
+            this.httpClient = httpClient;
+        }
+        public async override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            //first read the jwt of the user
+            var token = await js.GetFromLocalStorage(TOKENKEY);
+
+            if (string.IsNullOrEmpty(token))
+            { 
+                return anonymous; //authenticationstate
+            }
+
+            //return an authstate that indicates a user is logged in and has claims of the user
+            return BuildAuthenticationState(token);
+        }
+
+
+        public AuthenticationState BuildAuthenticationState(string token)
+        {
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", token);
+            return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity(ParseClaimsFromJwt(token), "jwt")));
+        }
+
+        private IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
+        {
+            var claims = new List<Claim>();
+            var payload = jwt.Split('.')[1];
+            var jsonBytes = ParseBase64WithoutPadding(payload);
+            var keyValuePairs = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonBytes);
+
+            keyValuePairs.TryGetValue(ClaimTypes.Role, out object roles);
+
+            if (roles != null)
+            {
+                if (roles.ToString().Trim().StartsWith("["))
+                {
+                    var parsedRoles = JsonSerializer.Deserialize<string[]>(roles.ToString());
+
+                    foreach (var parsedRole in parsedRoles)
+                    {
+                        claims.Add(new Claim(ClaimTypes.Role, parsedRole));
+                    }
+                }
+                else
+                {
+                    claims.Add(new Claim(ClaimTypes.Role, roles.ToString()));
+                }
+
+                keyValuePairs.Remove(ClaimTypes.Role);
+            }
+
+            claims.AddRange(keyValuePairs.Select(kvp => new Claim(kvp.Key, kvp.Value.ToString())));
+            return claims;
+        }
+
+        private byte[] ParseBase64WithoutPadding(string base64)
+        {
+            switch (base64.Length % 4)
+            {
+                case 2: base64 += "=="; break;
+                case 3: base64 += "="; break;
+            }
+            return Convert.FromBase64String(base64);
+        }
+
+        public async Task Login(string token)
+        {
+            await js.SetInLocalStorage(TOKENKEY, token);
+            var authState = BuildAuthenticationState(token);//we are going to build a new authentication state based on the token that is received
+            NotifyAuthenticationStateChanged(Task.FromResult(authState));
+        }
+
+        public async Task Logout()
+        {
+            await js.RemoveItem(TOKENKEY);
+        }
+    }
+}
