daeb68b049e40c0b2f7680c4b4e92eb2cbf7d71c
NjezeOj <47678586+NjezeOj@users.noreply.github.com>
2021-03-25 11:58:03 +0100

    UserName and CompanyId added

--- Opine/Shared/DTOS/UserInfo.cs
+++ Opine/Shared/DTOS/UserInfo.cs
@@ -6,9 +6,10 @@ namespace Opine.Shared.DTOS
 {
     public class UserInfo
     {
-        public string FullName { get; set; }
+        public string UserName { get; set; }
         public string Email { get; set; }
         public string Company { get; set; } 
         public string Password { get; set; }
+        public int CompanyId { get; set; }
     }
 }
