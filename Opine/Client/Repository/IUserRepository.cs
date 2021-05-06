a5054a5684d340f03c7b2a68eeed6cb1b0f6b4c4
NjezeOj <47678586+NjezeOj@users.noreply.github.com>
2021-05-06 23:55:30 +0100

    New

---
+++ Opine/Client/Repository/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using Opine.Shared.DTOS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Opine.Client.Repository
+{
+    public interface IUserRepository
+    {
+        Task AssignRole(EditRoleDTO editRoleDTO);
+        Task<List<RoleDTO>> GetRoles();
+        Task<PaginatedResponse<List<UserDTO>>> GetUsers(PaginationDTO paginationDTO);
+        Task RemoveRole(EditRoleDTO editRoleDTO);
+    }
+}
---
+++ Opine/Client/Repository/UserRepository.cs
@@ -0,0 +1,48 @@
+﻿using Opine.Client.Helpers;
+using Opine.Shared.DTOS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Opine.Client.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly IHttpService httpService;
+        private readonly string baseURL = "api/users";
+        public UserRepository(IHttpService httpService)
+        {
+            this.httpService = httpService;
+        }
+
+        public async Task<PaginatedResponse<List<UserDTO>>> GetUsers(PaginationDTO paginationDTO)
+        {
+            return await httpService.GetHelper<List<UserDTO>>(baseURL, paginationDTO);
+        }
+
+        public async Task<List<RoleDTO>> GetRoles()
+        {
+            return await httpService.GetHelper<List<RoleDTO>>($"{baseURL}/roles");
+        }
+
+        public async Task AssignRole(EditRoleDTO editRoleDTO)
+        {
+            var response = await httpService.Post($"{baseURL}/assignRole", editRoleDTO);
+            if (!response.Success)
+            {
+                throw new ApplicationException(await response.GetBody());
+            }
+        }
+
+        public async Task RemoveRole(EditRoleDTO editRoleDTO)
+        {
+            var response = await httpService.Post($"{baseURL}/assignRole", editRoleDTO);
+            if (!response.Success)
+            {
+                throw new ApplicationException(await response.GetBody());
+            }
+        }
+
+    }
+}
