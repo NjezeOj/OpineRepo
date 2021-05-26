ac4ec8a360de7a484502710b7e5e26c8a66d242c
NjezeOj <47678586+NjezeOj@users.noreply.github.com>
2021-05-27 00:35:51 +0100

    Poll Table changed

---
+++ Opine/Server/Migrations/20210526182904_PollTable.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Opine.Server.Migrations
+{
+    public partial class PollTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "A",
+                table: "Polls");
+
+            migrationBuilder.DropColumn(
+                name: "B",
+                table: "Polls");
+
+            migrationBuilder.DropColumn(
+                name: "C",
+                table: "Polls");
+
+            migrationBuilder.RenameColumn(
+                name: "D",
+                table: "Polls",
+                newName: "Count");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Option",
+                table: "Polls",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Option",
+                table: "Polls");
+
+            migrationBuilder.RenameColumn(
+                name: "Count",
+                table: "Polls",
+                newName: "D");
+
+            migrationBuilder.AddColumn<int>(
+                name: "A",
+                table: "Polls",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "B",
+                table: "Polls",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "C",
+                table: "Polls",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
