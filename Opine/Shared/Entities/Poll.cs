eeec00a768485eeb48942942bb66a079ab12aed1
NjezeOj <47678586+NjezeOj@users.noreply.github.com>
2021-05-27 00:39:16 +0100

    Updated

--- Opine/Shared/Entities/Poll.cs
+++ Opine/Shared/Entities/Poll.cs
@@ -7,10 +7,8 @@ namespace Opine.Shared.Entities
     public class Poll
     {
         public int Id { get; set; }
-        public int A { get; set; }
-        public int B { get; set; }
-        public int C { get; set; }
-        public int D { get; set; }
+        public string Option { get; set; }
+        public int Count { get; set; }
         public int QuestionId { get; set; }
         public Question Question { get; set; }
         
