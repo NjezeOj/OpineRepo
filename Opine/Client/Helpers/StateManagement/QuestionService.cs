8be9619245fb916aff85d167658c6efc994cfc37
NjezeOj <47678586+NjezeOj@users.noreply.github.com>
2021-05-27 00:36:09 +0100

    Updated

--- Opine/Client/Helpers/StateManagement/QuestionService.cs
+++ Opine/Client/Helpers/StateManagement/QuestionService.cs
@@ -1,16 +1,21 @@
-﻿using System;
+﻿using Opine.Shared.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Text.Json;
 
 namespace Opine.Client.Helpers.StateManagement
 {
     public class QuestionService
     {
+       
         public TimeSpan TimeCreated(DateTime uploadTime)
         {
             TimeSpan diff = DateTime.Now - uploadTime;
             return diff;
         }
+
+   
     }
 }
