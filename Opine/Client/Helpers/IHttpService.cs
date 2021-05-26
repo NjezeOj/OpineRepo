ab6abdb2bac90d36210921fe26d786b6832fe8e0
NjezeOj <47678586+NjezeOj@users.noreply.github.com>
2021-05-26 13:04:22 +0100

    Put Method added

--- Opine/Client/Helpers/IHttpService.cs
+++ Opine/Client/Helpers/IHttpService.cs
@@ -11,6 +11,6 @@ namespace Opine.Client.Helpers
         Task<HttpResponseWrapper<T>> Get<T>(string url);
         Task<HttpResponseWrapper<object>> Post<T>(string url, T data);
         Task<HttpResponseWrapper<TResponse>> Post<T, TResponse>(string url, T data);
-        
+        Task<HttpResponseWrapper<object>> Put<T>(string url, T data);
     }
 }
